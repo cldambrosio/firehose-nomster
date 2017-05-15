019cc0d7761211539ef99c06e8a444caad8d09a6
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-05-15 09:24:48 +0000

    additional validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50, minimum: 3 }
+  validates :address, :description, presence: true
 end
