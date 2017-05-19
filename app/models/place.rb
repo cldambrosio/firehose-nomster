bbff2fbb68d0281a04f669efae62388acf884c97
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-05-19 12:59:49 +0000

    set up db/model for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, presence: true, length: { maximum: 50, minimum: 3 }
   validates :address, :description, presence: true
 end
