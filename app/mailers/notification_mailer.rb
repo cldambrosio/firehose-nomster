9c32109b903cd5dbc8b6c3636e9baec6bbde3109
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-05-23 16:20:21 +0000

    lesson 41 previewing automated emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,9 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-def comment_added
-  mail(to: "claire.dambrosio@gmail.com", subject: "A comment has been added to your place")
-end
+  def comment_added
+    mail(to: "claire.dambrosio@gmail.com",
+     subject: "A comment has been added to your place")
+  end
 
 end
